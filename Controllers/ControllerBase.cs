84b9a2adc3bb6efba5a4c12c5bd984d2c53abd35
Daniel Lundholm <daniel.lundholm@dekiru.se>
2023-11-28 11:04:35 +0100

    Add project files.

---
+++ Controllers/ControllerBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class GenericControllerBase<T>: ControllerBase where T: EntityBase
+{
+    protected readonly DataContext _context;
+
+    public GenericControllerBase(DataContext dataContext)
+    {
+        _context = dataContext;
+    }
+}
+
---
+++ Models/EntityBase.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models;
+
+public class EntityBase
+{
+    public int Id { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Updated { get; set; }
+}
+
